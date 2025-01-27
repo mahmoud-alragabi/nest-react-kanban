5d646dc1ea833faeccd626a48edc98842da83b18
MedoAngel <actionman14.ma@gmail.com>
2025-01-27 15:28:42 +0100

    add CreateBoard to board DTOs

--- backend/src/board/dto/create-board.dto.ts
+++ backend/src/board/dto/create-board.dto.ts
@@ -1,1 +1,9 @@
-export class CreateBoardDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateBoardDto {
+  @ApiProperty({ example: 'My First Board' })
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+}
