690222170ead996df503ada993fa367df297dd67
MedoAngel <actionman14.ma@gmail.com>
2025-01-28 15:25:07 +0100

    add CreateTask to Task DTOs

--- backend/src/task/dto/create-task.dto.ts
+++ backend/src/task/dto/create-task.dto.ts
@@ -1,1 +1,20 @@
-export class CreateTaskDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTaskDto {
+  @ApiProperty({ example: 'My First Task' })
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty({
+    example: 1,
+    description: 'List ID to which this task belongs',
+  })
+  @IsInt()
+  listId: number;
+
+  @ApiProperty({ example: 1, description: 'Position of the task in the list' })
+  @IsInt()
+  position: number;
+}
