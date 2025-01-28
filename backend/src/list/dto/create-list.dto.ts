c8501193fd3a035bf966fafe856449a43bf2c03d
MedoAngel <actionman14.ma@gmail.com>
2025-01-28 14:32:50 +0100

    add CreateListDto to list DTOs

--- backend/src/list/dto/create-list.dto.ts
+++ backend/src/list/dto/create-list.dto.ts
@@ -1,1 +1,20 @@
-export class CreateListDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateListDto {
+  @ApiProperty({ example: 'To Do' })
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @ApiProperty({
+    example: 1,
+    description: 'Board ID to which this list belongs',
+  })
+  @IsInt()
+  boardId: number;
+
+  @ApiProperty({ example: 1, description: 'Position of the list in the board' })
+  @IsInt()
+  position: number;
+}
