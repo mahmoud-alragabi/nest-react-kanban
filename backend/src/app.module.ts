51ec2ec8fa8007528284bd6f3bf7e3e00671b5c8
MedoAngel <actionman14.ma@gmail.com>
2025-01-28 14:36:44 +0100

    add Task resource to backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,17 @@ import { KyselyModule } from './kysely/kysely.module';
 import { AuthModule } from './auth/auth.module';
 import { BoardModule } from './board/board.module';
 import { ListModule } from './list/list.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [UserModule, KyselyModule, AuthModule, BoardModule, ListModule],
+  imports: [
+    UserModule,
+    KyselyModule,
+    AuthModule,
+    BoardModule,
+    ListModule,
+    TaskModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
