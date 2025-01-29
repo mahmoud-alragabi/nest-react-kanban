c196a06d127f8b9e1b952485b09a9252a9a3277b
MedoAngel <actionman14.ma@gmail.com>
2025-01-29 20:20:08 +0100

    add tailwind to frontend

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content:['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: [],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
