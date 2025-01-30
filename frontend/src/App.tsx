10e41d31cf42288956b80c21a102636bd3c53e1b
MedoAngel <actionman14.ma@gmail.com>
2025-01-30 11:14:57 +0100

    add fontawesome icons to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {library} from "@fortawesome/fontawesome-svg-core"
+import {fas} from "@fortawesome/free-solid-svg-icons"
 import './App.css'
+
+library.add(fas)
 
 function App() {
   const [count, setCount] = useState(0)
