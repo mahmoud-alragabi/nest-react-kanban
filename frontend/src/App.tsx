ed8de638beed2d3eff05b3ea1ad980bf1ee913d4
MedoAngel <actionman14.ma@gmail.com>
2025-02-02 15:03:55 +0100

    add /kanban to routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import KanbanView from "./pages/Kanban/KanbanView";
 
 library.add(fas);
 
@@ -13,6 +14,7 @@ function App() {
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/kanban" element={<KanbanView />} />
       <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
