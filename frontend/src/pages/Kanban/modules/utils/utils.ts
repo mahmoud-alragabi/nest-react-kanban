80c0e8e5f68b584e9518528f104e73863591ec5b
MedoAngel <actionman14.ma@gmail.com>
2025-02-04 15:33:12 +0100

    add updateById to kanban utils module

--- frontend/src/pages/Kanban/modules/utils/utils.ts
+++ frontend/src/pages/Kanban/modules/utils/utils.ts
@@ -5,3 +5,18 @@ export type WithId = {
 export const removeById = <T extends WithId>(array: T[], id: T["id"]): T[] => {
   return array.filter((item) => item.id !== id);
 };
+
+export const updateById = <T extends WithId>(
+  array: T[],
+  newData: T,
+  id: T["id"],
+) => {
+  const arrayCopy = [...array];
+  const index = arrayCopy.findIndex((item) => item.id === id);
+
+  if (index < 0) throw new Error("item to update not found");
+
+  arrayCopy.splice(index, 1, newData);
+
+  return arrayCopy;
+};
