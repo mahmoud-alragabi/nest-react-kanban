ff621b28d5ae97dc424eee9c621692d73c524da2
MedoAngel <actionman14.ma@gmail.com>
2025-02-05 10:58:12 +0100

    remove global FontAwesome

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,8 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import KanbanView from "./pages/Kanban/KanbanView";
-
-library.add(fas);
 
 function App() {
   return (
