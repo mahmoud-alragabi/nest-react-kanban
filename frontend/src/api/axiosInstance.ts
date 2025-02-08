3fbf878719a56e2dfcaa071dd89580b585b7293b
MedoAngel <actionman14.ma@gmail.com>
2025-02-08 21:48:11 +0100

    add api url as env variable

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
 });
