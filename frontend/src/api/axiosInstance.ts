8d2d0d95cf58db89c139cfedbe30d98099385808
MedoAngel <actionman14.ma@gmail.com>
2025-02-08 22:55:23 +0100

    add new backend endpoint

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 
 export const axiosInstance: AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://nest-react-kanban-production.up.railway.app/",
   withCredentials: true,
 });
